f18988b548c103dd44c83efa415ea4b3488db68b
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2023-09-14 19:54:22 +0200

    Add files via upload

---
+++ InicijalnoPonavljanje7/InicijalnoPonavljanje7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj = 1, j;
+            for(int i = 3; i < 100; i++)
+            {
+            
+                for(j = 2; j < i; j++)
+                {
+                    if (i % j ==  0)
+                    {
+                        goto dalje;
+                    }
+                    else
+                    {
+                        if(j == i-1)
+                        {
+                            //Console.WriteLine(i);
+                            broj++;
+                        }
+                        
+                    }
+                }
+            dalje:;
+            }
+            Console.WriteLine("Broj primarnih brojeva manjih od 100: " + broj);
+            Console.ReadKey();
+        }
+    }
+}
